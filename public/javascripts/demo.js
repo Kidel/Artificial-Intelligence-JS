d2f4a38a72e603614664eefe504accefc8e8b67e
Kidel <tano.design@gmail.com>
2016-04-17 23:35:04 +0200

    some tuning here and there

--- public/javascripts/demo.js
+++ public/javascripts/demo.js
@@ -5,81 +5,79 @@ queens_puzzle.size = 20;
 queens_puzzle.get_initial_node = function(){
     var arr = [];
     for(var i = 0; i< queens_puzzle.size; i++){
-        arr[i] = 0;
-    }
-    return arr;
-};
-queens_puzzle.create_random_node = function() {
-    var arr = [];
-    for(var i=0; i<queens_puzzle.get_initial_node().length; i++){
         arr[i] = i;
     }
-    //random perturbation
-    for(var i=0; i<queens_puzzle.get_initial_node().length; i++){
-        this.randomswap(arr);
-    }
     return arr;
 };
-queens_puzzle.randomswap = function(arr){
-    var c1 = Math.floor((Math.random() * (queens_puzzle.size)));
-    var c2 = Math.floor((Math.random() * (queens_puzzle.size)));
+queens_puzzle.randomswap = function(c1, node){
+    var arr = clone(node);
+    var c2 = Math.floor((Math.random() * (node.length)));
     var app = arr[c1];
     arr[c1] = arr[c2];
     arr[c2] = app;
     return arr;
 };
-queens_puzzle.select_random_successor = function(node) {
-    var arr = [];
-    for(var i=0; i<node.length; i++){
-        arr[i] = node[i];
+queens_puzzle.create_random_node = function() {
+    var arr = queens_puzzle.get_initial_node();
+    //random perturbation
+    for(var j=0; j<100; j++) {
+        for (var i=0; i<arr.length; i++) {
+            arr = queens_puzzle.randomswap(i, arr);
+        }
     }
-    // randomly swap 2 columns
-    arr = queens_puzzle.randomswap(arr);
     return arr;
+};
+queens_puzzle.select_random_successor = function(node) {
+    var app = clone(node);
+    // randomly swap 2 columns
+    var rand_index = Math.floor((Math.random() * (node.length)));
+    return queens_puzzle.randomswap(rand_index, app);
 };
 queens_puzzle.evaluate = function(node) {
     var conflicts = 0;
+    // for each column/queen
     for(var c=0; c<node.length; c++){
+        // and again for each column
         for(var j=0; j<node.length; j++){
             if(c!=j) {
                 //counting queens on the same row
-                if (node[c] == node[c]) conflicts++;
+                if (node[j] == node[c]) conflicts++;
                 //counting queens on the same diagonal
-                if (c+1 < node.length && node[c] == node[c+1]-1) conflicts++;
-                if (c+1 < node.length && node[c] == node[c+1]+1) conflicts++;
-                if (c-1 >= 0 && node[c] == node[c-1]-1) conflicts++;
-                if (c-1 >= 0 && node[c] == node[c-1]+1) conflicts++;
+                if (j+1!=c && j+1 < node.length && node[c] == node[j+1]-1) conflicts++;
+                if (j+1!=c && j+1 < node.length && node[c] == node[j+1]+1) conflicts++;
+                if (j-1!=c && j-1 >= 0 && node[c] == node[j-1]-1) conflicts++;
+                if (j-1!=c && j-1 >= 0 && node[c] == node[j-1]+1) conflicts++;
             }
         }
     }
     return ((-1)*conflicts);
 };
 queens_puzzle.fitness = function(node) {
-    return (queens_puzzle.evaluate(node)+(queens_puzzle.size*(queens_puzzle.size-1)))/(queens_puzzle.size*(queens_puzzle.size-1));
+    return queens_puzzle.evaluate(node);
 };
-queens_puzzle.halting = function(node) {
+queens_puzzle.halting = function(evaluation) {
     // if we find a solution it's useless to go on in this kind of problem
-    return (queens_puzzle.evaluate(node) == 0 || queens_puzzle.evaluate(node) == (queens_puzzle.size*(queens_puzzle.size-1)));
+    return (evaluation == 0);
 };
 queens_puzzle.cooling = null;
 
 // example options
 var queens_options = {
-    "infinite_value": 10000,
-    "mutation_rate": 0.2,
-    "selection_rate": 0.05
+    "infinite_value": null,
+    "mutation_rate": 0.05,
+    "selection_rate": 0.1
 };
 
 
 function mushroom_demo() {
-    document.getElementById("status").innerHTML += display_time() + "Calculating simulated annealing...<br />";
+    /*document.getElementById("status").innerHTML += display_time() + "Calculating simulated annealing...<br />";
     document.getElementById("simulatedannealing").innerHTML = print_queens(simulated_annealing_simple(queens_puzzle, queens_options));
     document.getElementById("status").innerHTML += display_time() + "Done!<br />";
-/*
+
     document.getElementById("status").innerHTML += display_time() + "Calculating genetic algorithm...<br />";
-    document.getElementById("geneticalgorithm").innerHTML = print_queens(genetic_algorithm_simple(queens_puzzle, 200, queens_options));
-    document.getElementById("status").innerHTML += display_time() + "Done!<br />";
-*/
+    document.getElementById("geneticalgorithm").innerHTML = print_queens(genetic_algorithm_simple(queens_puzzle, 300, queens_options));
+    document.getElementById("status").innerHTML += display_time() + "Done!<br />";*/
+
     document.getElementById("status").innerHTML += display_time() + "Loading the mushrooms data set...<br />";
     getJSON("../mushrooms/", function(err, data) {
         if (err != null)
--- public/javascripts/localsearch.js
+++ public/javascripts/localsearch.js
@@ -6,18 +6,19 @@
  * returns a local min/max as a node for that problem
  */
 var simulated_annealing_simple = function(problem, options){
-    var MAX,
+    var infinite_value,
         cooling,
         halting;
 
     var current = problem.create_random_node();
+    console.log(current);
 
-    if(options == null || options.infinite_value == null) MAX = 10000;
-    else MAX = options.infinite_value;
+    if(options == null || options.infinite_value == null) infinite_value = 100000;
+    else infinite_value = options.infinite_value;
 
     if(problem.cooling == null) {
         cooling = function (t, infinite) {
-            return Math.floor(infinite-t);
+            return Math.floor(((infinite-t)/infinite)*50)+1;
         };
     }
     else cooling = problem.cooling;
@@ -27,22 +28,25 @@ var simulated_annealing_simple = function(problem, options){
     }
     else halting = problem.halting;
     // end of setup
-
+    var loops = 0;
     var temp = null;
-    for(var time = 1; time < MAX; time++){
+    for(var time = 1; time < infinite_value; time++){
         //checking if halting condition is matched
-        if(halting(current)) { console.log("annealing halt"); return current;  }
+        var evaluation = problem.evaluate(current);
+        if(halting(evaluation)) { console.log("annealing halt"); return current;  }
 
-        temp = cooling(time, MAX);
+        temp = cooling(time, infinite_value);
         if(temp == 0) return current;
         var next = problem.select_random_successor(current);
-        var d_e = problem.evaluate(next) - problem.evaluate(current);
+        var d_e = problem.evaluate(next) - evaluation;
         if(d_e >= 0) current = next;
         else {
-            var prob = Math.pow(Math.E, d_e/temp);
+            var prob = Math.pow(Math.E, d_e/temp)/2;
             var rand = Math.random();
             if (rand < prob) current = next;
+            //console.log("temp ", temp, " prob ", prob, " rand ", rand, " evaluation ", evaluation, " d_e ", d_e);
         }
+        loops++;
     }
     return current;
 };
@@ -51,7 +55,9 @@ var simulated_annealing_simple = function(problem, options){
  */
 var reproduction = function(node1, node2){
     // for each couple make 2 children with a random cut of the sequence
-    var cut = Math.floor(Math.random() * (node1.length)); //rand from 0 to last index
+    do {
+        var cut = Math.floor(Math.random() * (node1.length-1)); //rand from 0 to (last_index-1)
+    } while(node1[cut] == node2[cut+1]); // avoid copying some duplicates
     var app1 = [];
     var app2 = [];
     for(var i=0; i<node1.length; i++) {
@@ -70,7 +76,8 @@ var reproduction = function(node1, node2){
 
 /* given a node generates a random mutation in a random slot
  */
-var mutation = function(node) {
+var mutation = function(node, rate) {
+    if(Math.random() >= rate) return node;
     node[Math.floor(Math.random() * (node.length))] = Math.floor(Math.random() * (node.length));
     return node;
 };
@@ -81,66 +88,77 @@ var mutation = function(node) {
  * nodes can only be arrays of numbers for now. Can also include a halting function.
  * k: number of initial nodes
  * options: json format options to specify an infinite_value to loop time until that at best.
- *          Can also include mutation rate (from 0 to 1) and selection rate (from 0 to 1)
+ *          Can also include mutation rate (from 0 to 1, 0.1 meaning 10% mutation rate) and
+ *          a selection rate (from 0 to 0.5, 0.2 meaning that 20% will die every generation)
  *
  * returns a local min/max as a node for that problem
  */
-var genetic_algorithm_simple = function(problem, k, options){
-    var nodes = [];
-    var selections = [];
-    var cut = 1;
-    var MAX,
-        halting,
-        mutation_rate,
-        selection_rate;
-
-    for(var i=0; i<k; i++){
-        nodes[i] = problem.create_random_node();
-    }
-
-    if(options == null || options.infinite_value == null) MAX = 10000;
-    else MAX = options.infinite_value;
-    if(options == null || options.mutation_rate == null) mutation_rate = 10000;
+var genetic_algorithm_simple = function(problem, population_size, options) {
+    var catastrophe_free_generations = 0.10 * max_generations;
+    var current_generation; // counter
+    var max_generations;
+    var individuals = [];
+    var selection_rate;
+    var mutation_rate;
+    if (options == null || options.infinite_value == null) max_generations = 10000;
+    else max_generations = options.infinite_value;
+    if (options == null || options.mutation_rate == null) mutation_rate = 0.05;
     else mutation_rate = options.mutation_rate;
-    if(options == null || options.selection_rate == null) selection_rate = 10000;
+    if (options == null || options.selection_rate == null) selection_rate = 0.1;
     else selection_rate = options.selection_rate;
 
-    if(problem.halting == null) {
-        halting = function(foo) {return false};
+    function compare_fitness(a,b) {
+        if (a.fitness > b.fitness)
+            return -1;
+        else if (a.fitness < b.fitness)
+            return 1;
+        else
+            return 0;
     }
-    else halting = problem.halting;
-    // setup done
 
-    for(var j=0; j<MAX; j++) {
-        var exclusions = []; // to avoid that the fittest replaces every weak one
-        // natural selection killing 10% of the samples
-        for(var y = 0; y < Math.floor(k*selection_rate); y++) {
-            for (var i = 0; i < k; i++) {
-                selections[i] = problem.fitness(nodes[i]);
-            }
-            var strongest = get_index_and_max_value(selections, exclusions);
-            var weakest = get_index_and_min_value(selections, exclusions);
-            // killing the weakest
-            nodes[weakest.index] = nodes[strongest.index];
-            exclusions.push(strongest.index);
+    function create_individuals() {
+        for (var i = 0; i < population_size; i++) {
+            individuals[i] = {};
+            individuals[i].node = problem.create_random_node();
+            individuals[i].fitness = problem.fitness(individuals[i].node);
         }
-        //checking if halting condition is matched
-        if (halting(nodes[strongest.index])) { console.log("genetic halt"); return nodes[strongest.index];  }
-        // fixing odd number
-        if (!is_even(nodes.length)) nodes[nodes.length] = nodes[strongest.index];
-        // reproduction for each node
-        for(var i=0; i<nodes.length; i+=2){
-            // reproduction
-            var childs = reproduction(nodes[i], nodes[i+1]);
-            nodes[i] = childs[0];
-            nodes[i+1] = childs[1];
-            // mutation
-            if(Math.random()> 1 - mutation_rate) 
-                if(problem.fitness(nodes[i])<0) mutation(nodes[i]);
-            if(Math.random()> 1 - mutation_rate)
-                if(problem.fitness(nodes[i+1])<0) mutation(nodes[i+1]);
-        }
+        // select best, ordering by fitness
+        individuals.sort(compare_fitness);
     }
-    var best = get_index_and_max_value(selections);
-    return nodes[best.index];
+    create_individuals();
+    if (problem.halting(individuals[0].node)) { console.log("genetic halt"); return individuals[0].node; }
+    //setup done
+
+    for (current_generation = 0; current_generation <= max_generations; current_generation++) {
+        // catastrophe.
+        if ((current_generation > catastrophe_free_generations) && (Math.random() > 0.05)) {
+            catastrophe_free_generations += current_generation + catastrophe_free_generations;
+            create_individuals();
+        }
+
+        // create next generation with the fittest
+        var new_individuals = [];
+        for(var i=0; i<individuals.length; i+=2){
+            var index;
+            // excluding the weakest
+            if(i<Math.floor(individuals.length*(1-selection_rate)))
+                index = i;
+            else
+                index = i - Math.floor(individuals.length*(1-selection_rate));
+
+            var app = reproduction(individuals[index].node, individuals[index+1].node);
+            var brothers = [{}, {}];
+            for(var b in brothers){
+                // random mutation
+                brothers[b].node = mutation(app[0], mutation_rate);
+                brothers[b].fitness = problem.fitness(brothers[b].node);
+                new_individuals.push(brothers[b]);
+            }
+        }
+        new_individuals.sort(compare_fitness);
+        individuals = new_individuals;
+        if (problem.halting(individuals[0].node)) { console.log("genetic halt"); return individuals[0].node; }
+    }
+    console.log(individuals[0]);
+    return individuals[0].node;
 };
