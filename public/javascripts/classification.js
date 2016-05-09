1eaee5cd951956bef2c31b4dae25e6bd30b8c542
Kidel <tano.design@gmail.com>
2016-05-09 21:34:27 +0200

    now C4.5 correctly uses gain instead of plain entropy

--- public/javascripts/classification.js
+++ public/javascripts/classification.js
@@ -27,20 +27,25 @@ var majority_value = function(examples) {
 };
 
 
-/* returns the entropy value.
- * total: number of total elements
+/* returns the gain value.
+ * entropyS: entropy of the classification attribute
  * partial: array with the frequency of each attribute value, for each attribute
  *          like: [attrib1 => ['yes' => 4, 'no' => 5], attrib2 => [...], ...]
  */
-var entropy_array = function(partial, total) {
+var x = true;
+var gain_array = function(entropyS, partial) {
     var entropy = [];
     for(var a in partial) {
         entropy[a] = [];
         for(var v in partial[a]){
             entropy[a][v] = 0;
+            var total = 0;
+            for(var n in partial[a][v])
+                total += partial[a][v][n];
             for(var n in partial[a][v]) {
-                entropy[a][v] += (-1) * base2_log((partial[a][v][n] / total));
+                entropy[a][v] += (-1) * (partial[a][v][n] / total) * base2_log((partial[a][v][n] / total));
             }
+            entropy[a][v] = entropyS - entropy[a][v];
         }
     }
     return entropy;
@@ -94,9 +99,22 @@ var chose_attribute = function(attrib, examples) {
             partial[attrib[j]][examples[i][attrib[j]]][examples[i].classification]++;
         }
     }
-    var entropy = entropy_array(partial, total);
+    var classifications = [];
+    for(var i in examples) {
+        if(typeof classifications[examples[i].classification] == 'undefined') classifications[examples[i].classification] = 0;
+        classifications[examples[i].classification]++;
+    }
+    // Entropy(S)
+    var classification_entropy = 0;
+    for(var c in classifications) {
+        classification_entropy += (-1) * (classifications[c] / total) * base2_log((classifications[c] / total));
+    }
+    // Gain(S,Si) = Entropy(S) - Sum(Si/S * Entropy(Si), i)
+    var attribute_gain = gain_array(classification_entropy, partial);
+    //console.log("---------");
+    //console.log(attribute_gain);
     var best = {"name": "", "values": []};
-    best.name = index_of_best_value(entropy); // TODO: has to become gain instead of plain entropy
+    best.name = index_of_best_value(attribute_gain);
     best.values = get_possible_values(best.name, examples);
 
     return best;
@@ -169,51 +187,4 @@ var c4_5_simple = function(examples, attrib, def) {
         }
         return tree;
     }
-};
-
-
-/* Simple PRISM (offline version)
- * examples: a list of examples from the learning set, like [{attrib1: x, attrib2: y, ... , classification}, ... ]
- * attrib: a list of attributes to choose from, like ["attrib1", "attrib2", ... ]
- * def: a default value if there are no examples (left), like "yes", or 1.
- *
- * returns: a decision tree, like the one returned by c4_5_simple
- */
-var prism_offline = function(examples, attrib, def) {
-    function prism_offline_lim(examples, attrib, def, lim) {
-        if (lim<=0) return {
-            "label": majority_value(examples),
-            "type": "leaf",
-            "subtrees": [],
-            "note": "majority"
-        };
-        if (examples.length == 0) return {"label": def, "type": "leaf", "subtrees": []};
-        else if (same_classification(examples)) return {
-            "label": examples[0].classification,
-            "type": "leaf",
-            "subtrees": []
-        };
-        else if (attrib.length == 0) return {
-            "label": majority_value(examples),
-            "type": "leaf",
-            "subtrees": [],
-            "note": "majority"
-        };
-        else {
-            var best = chose_attribute(attrib, examples);
-            var tree = {"label": best.name, "subtrees": [], "type": "parameter"};
-            var m = majority_value(examples);
-            for (var i in best.values) {
-                var examples_i = filter_based_on_attrib_value(best.values[i], best.name, examples);
-                var subtree = {
-                    "label": best.values[i],
-                    "subtrees": [prism_offline_lim(examples_i, attrib, m, lim-1)],
-                    "type": "option"
-                };
-                tree.subtrees.push(subtree);
-            }
-            return tree;
-        }
-    }
-    return prism_offline_lim(examples, attrib, def, attrib.length*2);
 };
